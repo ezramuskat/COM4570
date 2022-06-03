ce5475c07d2be1fb5ccb97720ffac0425a01d370
Ezra Muskat <ezramuskat@gmail.com>
2022-06-02 23:58:07 -0400

    basic table functionality is up and running

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-let button = document.querySelector('button');
+let button = document.querySelector('submit');
+let glow = document.querySelector('glow');
+let table = document.querySelector('table');
 
 const addRow = () => {
-    let table = document.querySelector('table');
+    let name = document.querySelector('#name').value;
+    let birthday = document.querySelector('#birthday').value;
     let row = document.createElement('tr');
-    let cell = document.createElement('td');
-    let text = document.createTextNode('New Cell');
-    cell.appendChild(text);
-    row.appendChild(cell);
+    let nameCell = document.createElement('td');
+    let birthdayCell = document.createElement('td');
+    let nameText = document.createTextNode(name);
+    let birthdayText = document.createTextNode(birthday);
+    nameCell.appendChild(nameText);
+    birthdayCell.appendChild(birthdayText);
+    row.appendChild(nameCell);
+    row.appendChild(birthdayCell);
     table.appendChild(row);
 }
 
+const highlightNextBirthday = () => {
+
+}
+
 button.addEventListener('click', addRow);
+
